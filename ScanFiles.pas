f2f1b92af992366f34ff5b6473e12a8ca794f144
Paul Robinson <rfc1394@yahoo.com>
2021-11-22 12:55:06 -0500

    Proof of concept program to scan a directory and all subs below it and return a list of all files, with subdir name preceding.

---
+++ ScanFiles.pas
@@ -0,0 +1,14 @@
+{- $define Extensions}    // used by SortExtensions
+{$Define Files}           // used by ScanFiles
+program ScanFiles;
+{  Abstract:
+      Scans a directory and displays 
+      the names of all files 
+      in it and in all subdirectories
+      including subdirectory name, if
+      appropriate.   
+}
+
+{$I ScanSort.inc}
+
+
